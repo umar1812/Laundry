c8916db61cd052f67c0a7277ee1616c4056a22f7
umar1812 <umarsayyad1812@gmail.com>
2022-08-16 01:47:04 +0530

    Umar's 2nd commit

--- client/src/components/common/header.js
+++ client/src/components/common/header.js
@@ -1,35 +1,34 @@
 import "./header.css"
-// import { useNavigate } from "react-router-dom"
+import { NavLink } from 'react-router-dom'
 
 import logout from "../assests/logout-svgrepo-com.svg"
-const Header=()=>{
+const Header = () => {
 
-    // const navigate = useNavigate();
-    const handlelogout = ()=>{
-        localStorage.setItem("authorization","");
-        // navigate("/");
+    const handlelogout = () => {
+        localStorage.clear();
+        window.history.forward()
     }
     return (
         <>
-        <div className="header-main">
-        <div className="header-logo" >
-            <h1 className="header-name">Laundry</h1>
-        </div>
-        <div className="header-btn" >
-            <ul className="header-ul">
-                <li className="header-price"><p>Pricing</p></li>
-                <li className="header-career"><p>Career</p></li>
-                <div className="header-username">
-                <img className="header-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGvkLE9Ne2P3N_ZK-5vyXO4RKE3BDEe_26oA&usqp=CAU" alt="#"/>
-                <h3 className="user-p">User Name</h3>
-                <img className='logout-logo' src={logout} onClick={handlelogout} alt=""/>
-
+            <div className="header-main">
+                <div className="header-logo" >
+                    <h1 className="header-name">Laundry</h1>
                 </div>
-            
-                
-            </ul>
-        </div>
-        </div>
+                <div className="header-btn" >
+                    <ul className="header-ul">
+                        <li className="header-price"><p>Pricing</p></li>
+                        <li className="header-career"><p>Career</p></li>
+                        <div className="header-username">
+                            <img className="header-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGvkLE9Ne2P3N_ZK-5vyXO4RKE3BDEe_26oA&usqp=CAU" alt="#" />
+                            <h3 className="user-p">User Name</h3>
+                            <NavLink to={"/"} ><img className='logout-logo' src={logout} onClick={handlelogout} alt="logout" /></NavLink>
+
+                        </div>
+
+
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }
